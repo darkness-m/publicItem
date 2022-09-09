11bac37d09e2e994fcd9e3d7ee421015edfe9032
haoming.xu <haoming.xu@outlook.com>
2022-09-09 14:49:05 +0800

    feature(初始化) spring boot

---
+++ src/test/java/com/pubuli/item/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pubuli.item.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
